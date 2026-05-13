1976308c24b0aea9051b6a372b67002d6598f2b6
Muhammad Faturrohman <148172927+mfaturrohman@users.noreply.github.com>
2026-05-13 14:36:00 +0700

    a

--- preprocessing/automate_Muhammad-Faturrohman.py
+++ preprocessing/automate_Muhammad-Faturrohman.py
@@ -1,47 +1,49 @@
 import pandas as pd
-import numpy as np
 from sklearn.preprocessing import LabelEncoder
 import os
 
-def preprocess_data(input_path, output_path):
-    """
-    Fungsi utama untuk membaca data mentah, membersihkan, 
-    mengubah kategori menjadi angka, dan menyimpan hasilnya.
-    """
-    print(f"Memuat data dari: {input_path}")
-    
-    # Load Data
-    try:
-        df = pd.read_csv(input_path)
-    except FileNotFoundError:
-        print(f"File tidak ditemukan di {input_path}")
-        return
 
-    # Data Cleaning 
-    df = df.replace('?', np.nan)
-    
-    # Mengisi missing values dengan Modus (Nilai terbanyak)
+def load_data(path):
+    return pd.read_csv(path)
+
+
+def remove_duplicates(df):
+    return df.drop_duplicates()
+
+
+def encode_categorical(df):
+    encoder_dict = {}
+
     for col in df.columns:
-        if df[col].isnull().sum() > 0:
-            modus = df[col].mode()[0]
-            df[col] = df[col].fillna(modus)
-            print(f"Mengisi missing value di kolom '{col}' dengan modus: {modus}")
+        le = LabelEncoder()
+        df[col] = le.fit_transform(df[col])
+        encoder_dict[col] = le
 
-    # Encoding 
-    le = LabelEncoder()
-    for col in df.columns:
-        if df[col].dtype == 'object':
-            df[col] = le.fit_transform(df[col])
-    
-    print("Encoding selesai.")
+    return df, encoder_dict
 
-    # Saving Data
+
+def save_data(df, output_path):
+    # membuat folder jika belum ada
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
-    
-    df.to_csv(output_path, index=False)
-    print(f"Data bersih berhasil disimpan ke: {output_path}")
 
-if __name__ == "__main__":
-    INPUT_FILE = 'mushroom_raw/mushrooms.csv' 
-    OUTPUT_FILE = 'preprocessing/mushrooms_preprocessing/mushroom_clean.csv'
-    preprocess_data(INPUT_FILE, OUTPUT_FILE)
+    df.to_csv(output_path, index=False)
+
+
+if __name__ == '__main__':
+
+    input_path = 'mushrooms_raw/mushrooms.csv'
+    output_path = 'mushrooms_preprocessing/mushrooms_clean.csv'
+
+    # Load data
+    df = load_data(input_path)
+
+    # Remove duplicate
+    df = remove_duplicates(df)
+
+    # Encoding
+    df, encoders = encode_categorical(df)
+
+    # Save processed data
+    save_data(df, output_path)
+
+    print('Preprocessing selesai')
